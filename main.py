16788adb26a6c3bcad08f65e1e41ba94ef7ef361
landon <232344557@qq.com>
2022-07-02 22:18:40 +0800

    add gunicorn

--- main.py
+++ main.py
@@ -10,11 +10,17 @@ from forms import CreatePostForm, RegisterFrom, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 Base = declarative_base()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+print(os.environ.get('SECRET_KEY'))
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
